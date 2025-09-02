a0b83d56fea614ef2bc96a74b0f300d45f2c9400
kkoma2623 <jiwook.dev@gmail.com>
2025-09-02 23:02:21 +0900

    add code to test get DAO

--- src/main/java/me/connan/springbootdeveloper/TestController.java
+++ src/main/java/me/connan/springbootdeveloper/TestController.java
@@ -1,12 +1,19 @@
 package me.connan.springbootdeveloper;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	@Autowired
+	TestService testService;
+
 	@GetMapping("/test")
-	public String test() {
-		return "Hello World";
+	public List<Member> getAllMembers() {
+		List<Member> members = testService.getAllMembers();
+		return members;
 	}
 }
---
+++ src/main/java/me/connan/springbootdeveloper/TestService.java
@@ -0,0 +1,17 @@
+package me.connan.springbootdeveloper;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	public List<Member> getAllMembers() {
+		return memberRepository.findAll();
+	}
+}
