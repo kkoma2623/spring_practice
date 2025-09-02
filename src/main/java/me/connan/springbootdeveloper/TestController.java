c828de81f50ac2fec61b9efd256dca641875c722
kkoma2623 <jiwook.dev@gmail.com>
2025-09-02 22:36:06 +0900

    add TestController for GetMapping test

--- src/main/java/me/connan/springbootdeveloper/TestController.java
+++ src/main/java/me/connan/springbootdeveloper/TestController.java
@@ -1,4 +1,12 @@
 package me.connan.springbootdeveloper;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+	@GetMapping("/test")
+	public String test() {
+		return "Hello World";
+	}
 }
