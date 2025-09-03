068f24db398a6ec19b61d458df239b57cc24d2df
kkoma2623 <jiwook.dev@gmail.com>
2025-09-03 14:51:13 +0900

    JpaRepository test env

---
+++ src/test/java/me/connan/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package me.connan.springbootdeveloper;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Sql("/insert-members.sql")
+	@Test
+	void getAllMembers() {
+		// when
+		List<Member> members = memberRepository.findAll();
+
+		// then
+		assertThat(members.size()).isEqualTo(3);
+	}
+
+	@Sql("/insert-members.sql")
+	@Test
+	void getMemberById() {
+		// when
+		Member member = memberRepository.findById(2L)
+			.orElse(null);
+
+		// then
+		assert member != null;
+		assertThat(member.getName()).isEqualTo("B");
+	}
+}
